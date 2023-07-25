eda736c7bdb294b9ed5ee73dd9d81946afef66a6
Steven Tang <18170455+steventango@users.noreply.github.com>
2023-07-25 11:44:57 -0600

    Use TD3

--- examples/train_jsrl.py
+++ examples/train_jsrl.py
@@ -1,34 +1,34 @@
 import gymnasium as gym
-from stable_baselines3 import SAC
+from stable_baselines3 import TD3
 from stable_baselines3.common.callbacks import EvalCallback
 import numpy as np
 from jsrl import get_jsrl_algorithm
 
 
 def main():
-    max_horizon = 100
-    env = gym.make("PointMaze_UMaze-v3", continuing_task=False, max_episode_steps=max_horizon)
-    guide_policy = SAC.load("examples/models/sac_pointmaze_guide/best_model").policy
+    env = gym.make("PointMaze_UMaze-v3", continuing_task=False, max_episode_steps=150)
+    guide_policy = TD3.load("examples/models/pointmaze_guide_TD3/best_model").policy
     n = 10
-    model = get_jsrl_algorithm(SAC)(
+    max_horizon = 60
+    model = get_jsrl_algorithm(TD3)(
         "MultiInputPolicy",
         env,
         policy_kwargs=dict(
             guide_policy=guide_policy,
             max_horizon=max_horizon,
-            horizons=np.arange(max_horizon, -1, -max_horizon // n,),
-            tolerance=0.0,
+            horizons=np.arange(max_horizon, -1, -max_horizon // n,)
         ),
         verbose=1,
-        tensorboard_log="logs"
+        tensorboard_log="logs/pointmaze_jsrl"
     )
     model.learn(
-        total_timesteps=1e6,
+        total_timesteps=1e5,
         log_interval=10,
         progress_bar=True,
         callback=EvalCallback(
             env,
-            best_model_save_path="examples/models/sac_pointmaze_jsrl"
+            n_eval_episodes=100,
+            best_model_save_path="examples/models/pointmaze_jsrl_TD3"
         ),
     )
 
--- examples/train_scratch.py
+++ examples/train_scratch.py
@@ -1,15 +1,15 @@
 import gymnasium as gym
-from stable_baselines3 import SAC
+from stable_baselines3 import TD3
 from stable_baselines3.common.callbacks import EvalCallback
 
 
 def main():
     env = gym.make("PointMaze_UMaze-v3", continuing_task=False, max_episode_steps=150)
-    model = SAC(
+    model = TD3(
         "MultiInputPolicy",
         env,
         verbose=1,
-        tensorboard_log="logs"
+        tensorboard_log="logs/pointmaze_scratch"
     )
     model.learn(
         total_timesteps=1e6,
@@ -17,7 +17,8 @@ def main():
         progress_bar=True,
         callback=EvalCallback(
             env,
-            best_model_save_path="examples/models/sac_pointmaze_scratch"
+            n_eval_episodes=100,
+            best_model_save_path="examples/models/pointmaze_scratch_TD3"
         ),
     )
 
